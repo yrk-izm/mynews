5b3bbd6eb314a07d2976e240945a9fa73b870ec5
yurika <urk813@gmail.com>
2023-10-22 04:48:19 +0000

    laravel 08-10

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// laravel09
+use App\Http\Controllers\Admin\NewsController;
+Route::controller(NewsController::class)->prefix('admin')->group(function() {
+    Route::get('news/create', 'add');
+});
+
+// 課題？
+// use App\Http\Controllers\Admin\NewsController;
+// Route::controller(NewsController::class)->prefix('admin')->group(function() {
+//     Route::get('admin/profile/create','Admin\ProfileController@add');
+//     Route::get('admin/profile/edit','Admin\ProfileController@edit');
+// });
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-use App\Http\Controllers\Admin\NewsController;
-Route::controller(NewsController::class)->prefix('admin')->group(function() {
-    Route::get('admin/profile/create','Admin\ProfileController@add');
-    Route::get('admin/profile/edit','Admin\ProfileController@edit');
-});
