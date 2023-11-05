874f8b5311c5fa61297d9f6e724732a9d0f38284
yurika <urk813@gmail.com>
2023-11-05 06:29:30 +0000

    Laravel14practice2

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -36,7 +36,7 @@
                     <div class="form-group row">
                         <label class="col-md-2">自己紹介</label>
                         <div class="col-md-10">
-                            <textarea class="form-control" name="body" rows="20">{{ old('introduction') }}</textarea>
+                            <textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
                         </div>
                      </div>
                     @csrf
